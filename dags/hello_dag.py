7d2115ef3603211038915add5ae51d67210dbf9a
Hüseyin Yiğit Ülker <100729184+huseyin-yigit-ulker@users.noreply.github.com>
2025-07-09 11:49:17 +0300

    Update hello_dag.py

--- dags/hello_dag.py
+++ dags/hello_dag.py
@@ -1,18 +1,34 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
+import logging
 from datetime import datetime
+from typing import Dict
 
-def say_hello():
-    print("👋 Hello from Git + Astro!")
+import requests
+from airflow.sdk import dag, task
 
-with DAG(
-    dag_id="hello_git_astro",
-    start_date=datetime(2024, 1, 1),
-    schedule_interval="@daily",
-    catchup=False,
-    tags=["demo"]
-) as dag:
-    hello = PythonOperator(
-        task_id="print_hello",
-        python_callable=say_hello
-    )
+API = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true"
+
+
+@dag
+def taskflow():
+    @task(task_id="extract", retries=2)
+    def extract_bitcoin_price() -> Dict[str, float]:
+        return requests.get(API).json()["bitcoin"]
+
+    @task(multiple_outputs=True)
+    def process_data(response: Dict[str, float]) -> Dict[str, float]:
+        logging.info(response)
+        return {"usd": response["usd"], "change": response["usd_24h_change"]}
+
+    @task
+    def store_data(data: Dict[str, float]):
+        logging.info(f"Store: {data['usd']} with change {data['change']}")
+
+    _extract_bitcoin_price = extract_bitcoin_price()
+    _process_data = process_data(_extract_bitcoin_price)
+    store_data(_process_data)
+
+    # alternative in one line:
+    # store_data(process_data(extract_bitcoin_price()))
+
+
+taskflow()
